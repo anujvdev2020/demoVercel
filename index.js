0624d57399612e95515371f3952eb8f8f3d12db7
Ajay Batham <ajay.b@protonshub.com>
2022-10-19 11:18:41 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from 'express'
+
+const app=express()
+const port=9000
+
+app.use('/',(req,res)=>{
+    res.json({message:"Hello from express"})
+})
+app.listen(port,()=>{
+    console.log("I am live")
+})
